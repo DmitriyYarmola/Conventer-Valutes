0b0becc94c58d1573193b8a7e4321f1e5f3603ce
Dmitriy <dmitriy.yarmola@gmail.com>
2020-05-18 22:03:17 +0300

    Updates

---
+++ precache-manifest.6cd29b91d1e321323d58727db138697a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8167ea93061ef3c5457aabad1b794a7d",
+    "url": "/EnemyDeaD/ConventerValutes/index.html"
+  },
+  {
+    "revision": "15a4e02d7eb4b49b2185",
+    "url": "/EnemyDeaD/ConventerValutes/static/css/main.0dc043ca.chunk.css"
+  },
+  {
+    "revision": "a4b1faf482d88e16e349",
+    "url": "/EnemyDeaD/ConventerValutes/static/js/2.1327ae64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/EnemyDeaD/ConventerValutes/static/js/2.1327ae64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a4e02d7eb4b49b2185",
+    "url": "/EnemyDeaD/ConventerValutes/static/js/main.a8714419.chunk.js"
+  },
+  {
+    "revision": "a2208eb7ec5dd57ede6a",
+    "url": "/EnemyDeaD/ConventerValutes/static/js/runtime-main.13fe34b1.js"
+  }
+]);
