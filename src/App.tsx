23bfc6cd96885fe4bc8711bfd6193de28417b241
Dmitriy <dmitriy.yarmola@gmail.com>
2020-06-03 11:03:11 +0300

    ad eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useCallback } from 'react';
-import './App.sass';
-import { CurrentCurses } from './component/CurrentCurses/currentCurses';
-import { useSelector, useDispatch } from 'react-redux';
-import { AppStateType } from './stateManager/redux-store';
-import { getValutes } from './stateManager/valutes-reducer';
-import { Switch, Route } from 'react-router';
-import { ConvertorPage } from './component/ConventorPage/conventerPage';
+import React, { useEffect, useCallback } from "react";
+import "./App.sass";
+import { CurrentCurses } from "./component/CurrentCurses/currentCurses";
+import { useSelector, useDispatch } from "react-redux";
+import { AppStateType } from "./stateManager/redux-store";
+import { getValutes } from "./stateManager/valutes-reducer";
+import { Switch, Route } from "react-router";
+import { ConvertorPage } from "./component/ConventorPage/conventerPage";
 
 export const App = () => {
-
   /* ===UseSelector=== */
-  const currentValutes = useSelector((state: AppStateType) => state.valutesReducer.currentValutes)
+  const currentValutes = useSelector(
+    (state: AppStateType) => state.valutesReducer.currentValutes
+  );
 
   /* ===UseDispatcj=== */
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const getCurrentValutes = useCallback(() => {
-    dispatch(getValutes())
-  }, [dispatch])
+    dispatch(getValutes());
+  }, [dispatch]);
 
   useEffect(() => {
-    getCurrentValutes()
-  }, [])
+    getCurrentValutes();
+  }, []);
 
   return (
     <div className="container">
       <Switch>
-        <Route path="/" exact render={() => <ConvertorPage />}/>
-        <Route path="/currentCurses" render={() => <CurrentCurses />}/>
+        <Route path="/" exact render={() => <ConvertorPage />} />
+        <Route path="/currentCurses" render={() => <CurrentCurses />} />
       </Switch>
     </div>
   );
-}
-
+};
